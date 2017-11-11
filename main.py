38452c49062b91a0e46d08df257e1f66cca1fcd7
Aaron <aaron@aagavin.ca>
2017-11-11 04:44:38 +0000

    added cors for heroku

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify({'success': 'Hello World!'})
+
+
+CORS(app)
 
 
 if __name__ == '__main__':
