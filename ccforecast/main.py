22ef497106ebd98e9d5a5ca50e78e9d5db30e14f
leontibrechko <leontibrechko@gmail.com>
2017-11-11 22:54:07 -0500

    implemented basic polynomial prediction

--- ccforecast/main.py
+++ ccforecast/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from .metric_prediction import predict
 
 app = Flask(__name__)
 
@@ -9,8 +10,29 @@ def hello_world():
     return jsonify({'success': 'Hello World!'})
 
 
-CORS(app)
+@app.route('/predictPlease', methods=['POST'])
+def predict_metric():
+    if request.json is not None:
+        data = request.json['data']
+        predict_days = request.json['predictDays']
 
+        stats = list(list(data.values())[0].keys())
+        predictions = {}
+
+        for s in stats:
+            predict_stats = {}
+
+            for item in list(data.items()):
+                predict_stats[item[0]] = item[1][s]
+
+            predictions[s] = predict(predict_stats, predict_days)
+
+        return jsonify(predictions)
+    else:
+        return jsonify({'error': 'No date provided'})
+
+
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=False)
---
+++ ccforecast/metric_prediction.py
@@ -0,0 +1,42 @@
+import numpy as np
+from datetime import datetime, timedelta
+
+
+def predict(stats, predict_days):
+    latest_date = datetime(1970, 1, 1)
+
+    dates = [(__get_datetime(date), date) for date in list(stats.keys())]
+    dates = sorted(dates, key=lambda d: d[0])
+
+    timestamps = []
+    values = []
+
+    for date in dates:
+        latest_date = max(latest_date, date[0])
+
+        timestamps.append(__get_timestamp(date[0]))
+        values.append(stats[date[1]])
+
+    # order = max((len(dates) + 1) // 2, 5)
+    order = 4
+    predict_f = np.poly1d(np.polyfit(timestamps, values, order))
+
+    predict_days = [__get_timestamp(latest_date + timedelta(days=i)) for i in range(1, predict_days + 1, 1)]
+
+    # __debug(timestamps + predict_days, values + list(predict_f(predict_days)))
+    return list(predict_f(predict_days))
+
+
+def __get_datetime(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+def __get_timestamp(datetime):
+    return datetime.timestamp()
+
+
+def __debug(x, y):
+    import matplotlib.pyplot as plt
+
+    plt.plot(x, y)
+    plt.savefig('debug')
